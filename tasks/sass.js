5f515738bdf55ff840fa6ac9bf75e7c4be941a61
David Eisner <david.eisner@oriel.oxon.org>
2014-03-10 19:44:52 +0000

    Source maps for combined source files
    using an ugly temporary scss file in cwd to work around the lack of support for source maps of inline scss in node-sass/libsass

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,4 +1,6 @@
 'use strict';
+var fs = require('fs');
+var crypto = require('crypto');
 var path = require('path');
 var eachAsync = require('each-async');
 var chalk = require('chalk');
@@ -28,41 +30,59 @@ module.exports = function (grunt) {
 			if (path.basename(el.dest)[0] === '_') {
 				return next();
 			}
+			var cleanup = function() {};
+			try {
+				var renderOpts = {
+					success: function (css, map) {
+						grunt.file.write(el.dest, css);
+						grunt.log.writeln('File ' + chalk.cyan(el.dest) + ' created.');
 
-			var renderOpts = {
-				success: function (css, map) {
-					grunt.file.write(el.dest, css);
-					grunt.log.writeln('File ' + chalk.cyan(el.dest) + ' created.');
+						if (map) {
+							grunt.file.write(el.dest + '.map', map)
+							grunt.log.writeln('File ' + chalk.cyan(el.dest + '.map') + ' created.');
+						}
 
-					if (map) {
-						grunt.file.write(el.dest + '.map', map)
-						grunt.log.writeln('File ' + chalk.cyan(el.dest + '.map') + ' created.');
-					}
+						cleanup();
 
-					next();
-				},
-				error: grunt.warn,
-				includePaths: options.includePaths,
-				outputStyle: options.outputStyle,
-				sourceComments: options.sourceComments
-			};
+						next();
+					},
+					error: function(reason) {
+						grunt.warn(reason);
+						cleanup();
+					},
+					includePaths: options.includePaths,
+					outputStyle: options.outputStyle,
+					sourceComments: options.sourceComments
+				};
 
-			if (el.src.length > 1) {
-				renderOpts.data = '@import "' + el.src.join('";\n@import "') + '";\n';
-			} else {
-				renderOpts.file = el.src[0];
-			}
-
-			if (options.sourceMap) {
-				if (options.sourceMap === true) {
-					renderOpts.sourceMap = el.dest + '.map';
+				if (el.src.length > 1) {
+					var data = '@import "' + el.src.join('";\n@import "') + '";\n';
+					var src = '.tmp.' + crypto.createHash('sha1').update(data).digest("hex") + '.scss';
+					cleanup = function() {
+						fs.unlink(src, function(err) {
+							if (err) grunt.warn(err);
+						});
+					};
+					fs.writeFileSync(src, data);
+					renderOpts.file = src;
 				} else {
-					renderOpts.sourceMap = path.resolve(cwd, options.sourceMap);
+					renderOpts.file = el.src[0];
 				}
+
+				if (options.sourceMap) {
+					if (options.sourceMap === true) {
+						renderOpts.sourceMap = el.dest + '.map';
+					} else {
+						renderOpts.sourceMap = path.resolve(cwd, options.sourceMap);
+					}
+				}
+
+				sass.render(renderOpts);
+
+			} catch(e) {
+				cleanup();
+				throw e;
 			}
-
-			sass.render(renderOpts);
-
 		}, this.async());
 	});
 };
